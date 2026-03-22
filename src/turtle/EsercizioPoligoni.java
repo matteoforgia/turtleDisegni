273a518b423b120904e9c86f6e36caaa5f3e6d25
matteoforgia <matteo.forgiarini28@gmail.com>
2026-03-22 19:01:00 +0100

    finalmente ho finito di scrivere il codice per disegnare i poligoni con la turtle

--- src/turtle/EsercizioPoligoni.java
+++ src/turtle/EsercizioPoligoni.java
@@ -2,43 +2,58 @@ package turtle;
 
 import java.util.Scanner;
 
-public class EsercizioPoligoni extends TurtleScreen{
-    public EsercizioPoligoni() {
-        super(800, 600); // Dimensioni finestra
+public class EsercizioPoligoni extends TurtleScreen {
+
+    int N, K; // variabili globali
+
+    public EsercizioPoligoni(int N, int K) {
+        super(800, 600);
+        this.N = N;
+        this.K = K;
     }
+
     @Override
     public void setup() {
         noLoop();
+        disegnaPoligoni(N, K);
     }
-    public static void disegnaPoligoni(int n, int k) {
-        Turtle t = new Turtle(getInstance());
+
+    public void disegnaPoligoni(int n, int k) {
+        // TurtleScreen screen = getInstance();
+        Turtle t = new Turtle(this);
+
         t.speed(1);
         t.setPenSize(1);
+        t.penDown();
+        t.setPosition(0, 0);
+
         for (int i = 0; i < k; i++) {
             for (int j = 0; j < n; j++) {
-                t.forward(100); // Avanza di 100 pixel
-                t.left(360.0 / n); // Ruota a sinistra di un angolo basato sul numero di lati
+                t.forward(100); // lato più piccolo
+                t.left(360.0 / n);
             }
-            t.left(360.0 / k); // Ruota a sinistra per posizionare il prossimo poligono
+            t.left(360.0 / k); // ruota per il prossimo poligono
         }
-        t.hideTurtle(); // Nasconde la tartaruga alla fine
     }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         System.out.println("dammi un numero di lati compreso tra 3 e 12");
         int N = in.nextInt();
         if (N < 3 || N > 12) {
             System.out.println("Numero non valido. Riprova.");
             return;
         }
+
         System.out.println("dimmi quanti poligoni stampare, possibilmente minore di 100");
         int K = in.nextInt();
-        if (K>=100) {
+        if (K >= 100) {
             System.out.println("mi sembra un po' troppo");
             return;
         }
-        EsercizioPoligoni a = new EsercizioPoligoni();
-        a.run();
-        disegnaPoligoni(N, K);
+
+        EsercizioPoligoni disegno = new EsercizioPoligoni(N, K);
+        disegno.run();
     }
 }
